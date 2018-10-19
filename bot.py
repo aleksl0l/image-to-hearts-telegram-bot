435a7df2914ae7deb5a2b3b135cc7b424444030f
aleksl0l <aleksl0l@yandex.ru>
2018-10-19 14:31:23 +0300

    Refactoring

--- bot.py
+++ bot.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 
-from telegram.ext import Updater
+import os
+
+from PIL import Image
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
-import logging
-import os
-from img_to_hearts import img_to_hearts
-from PIL import Image
-from daemon import runner
+from telegram.ext import Updater
 
 import pref
+from img_to_hearts import img_to_hearts
+
 if pref.token_path:
     token_path = pref.token_path
 else:
-    token_path = os.getcwd() + '//token' 
+    token_path = os.getcwd() + '/token'
 images_path = pref.image_path
 
-
 count = 0
+
+
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Я бот, который сделает из фото сердечное фото! Отошли мне изображение, чтобы попробовать!")
+    msg = "Я бот, который сделает из фото изображение из сердечек! Отошли мне изображение, чтобы попробовать!"
+    bot.send_message(chat_id=update.message.chat_id,
+                     text=msg)
+
 
 def img_to_heart(bot, update):
     global count
@@ -27,72 +31,53 @@ def img_to_heart(bot, update):
     chat_id = update.message.chat_id
     print(file_id)
     bot.send_message(chat_id=chat_id, text="Класс! Ты прислал картинку!")
-    #обработать
-    newFile = bot.get_file(file_id)
-    newFile.download(images_path + file_id + '.jpg')
+    # обработать
+    new_file = bot.get_file(file_id)
+    new_file.download(images_path + file_id + '.jpg')
 
     img = img_to_hearts(Image.open(images_path + file_id + '.jpg'))
-    img.save(images_path + 'hearted_'+ file_id +'.jpg')
-    # 
-    # data = None
-    # with io.BytesIO() as output:
-    #     img.save(output, 'PNG')
-    #     data = output.getvalue()
-    # 
-    # imgByteArr = io.BytesIO()
-    # img.save(imgByteArr, format='PNG')
-    # imgByteArr = imgByteArr.getvalue()
-
-    #залить и переслать
-    bot.send_photo(chat_id=chat_id, photo=open(images_path + 'hearted_'+ file_id +'.jpg', 'rb'))
-    # bot.send_photo(chat_id=chat_id, photo=img.getdata())
+    img.save(images_path + 'hearted_' + file_id + '.jpg')
+    # залить и переслать
+    bot.send_photo(chat_id=chat_id, photo=open(images_path + 'hearted_' + file_id + '.jpg', 'rb'))
     count += 1
 
 
-class Main():
-    def run(self):
-        with open(token_path, 'r') as myfile:
-                token = myfile.read()
-        token = token[:-1]
-        updater = Updater(token=token)
-        
-        dispatcher = updater.dispatcher
-        
-        # logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-        
-        start_handler = CommandHandler('start', start)
-        img_handler = MessageHandler(Filters.photo, img_to_heart)
-        
-        dispatcher.add_handler(start_handler)
-        dispatcher.add_handler(img_handler)
-        updater.start_polling()
-        
-
-class App():
-    def __init__(self):
-        self.stdin_path = pref.stdin_path
-        self.stdout_path = pref.stdout_path
-        self.stderr_path = pref.stderr_path
-        self.pidfile_path =  pref.pidfile_path
-        self.pidfile_timeout = pref.pidfile_timeout
-            
-    def run(self):
-        service = Main()
-        service.run()
-        
-app = App()
-# logger = logging.getLogger("DaemonLog")
-# logger.setLevel(logging.INFO)
-# formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-# handler = logging.FileHandler("/var/log/testdaemon/testdaemon.log")
-# handler.setFormatter(formatter)
-# logger.addHandler(handler)
-
-daemon_runner = runner.DaemonRunner(app)
-daemon_runner.do_action()
-        
-        
+# class Main:
+#     def run(self):
+#         # with open(token_path, 'r') as myfile:
+#         #     token = myfile.read()
+#
+#
+#
+# class App:
+#     def __init__(self):
+#         self.stdin_path = pref.stdin_path
+#         self.stdout_path = pref.stdout_path
+#         self.stderr_path = pref.stderr_path
+#         self.pidfile_path = pref.pidfile_path
+#         self.pidfile_timeout = pref.pidfile_timeout
+#
+#     def run(self):
+#         service = Main()
+#         service.run()
 
 
+# app = App()
+# app.run()
+token = "405588645:AAFzHcmUPOyaW2tVucg2YGPX--V41hJfHT4"
+REQUEST_KWARGS={
+    'proxy_url': 'socks5://localhost:9050',
+}
+updater = Updater(token=token, request_kwargs=REQUEST_KWARGS)
 
+dispatcher = updater.dispatcher
 
+start_handler = CommandHandler('start', start)
+img_handler = MessageHandler(Filters.photo, img_to_heart)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(img_handler)
+updater.start_polling()
+updater.idle()
+# daemon_runner = runner.DaemonRunner(app)
+# daemon_runner.do_action()
--- img_to_hearts.py
+++ img_to_hearts.py
@@ -1,91 +1,80 @@
 #!/usr/bin/env python3
 
-from PIL import Image
-import numpy as np
-from random import shuffle
 import sys
-import os
+
+import numpy as np
+from PIL import Image
+
+heart_size = 10, 10
+im = {
+    'black': Image.open('./hearts/black4.png'),
+    'blue': Image.open('./hearts/blue.png'),
+    'red': Image.open('./hearts/red.png'),
+    'purple': Image.open('./hearts/purple.png'),
+    'yellow': Image.open('./hearts/yellow.png'),
+    'green': Image.open('./hearts/green.png')
+}
+
+mean = {
+    'black': np.array([29.05, 27.42, 36.87]),
+    'blue': np.array([20.13, 99.44, 174.79]),
+    'red': np.array([162.48, 33.22, 32.08]),
+    'purple': np.array([104.5, 23.92, 131.09]),
+    'yellow': np.array([197.36, 159.94, 40.95]),
+    'green': np.array([89.02, 134.15, 49.83])
+}
 
 
-def getAverageRGBN(image):
-    """
-    Given PIL Image, return average value of color as (r, g, b)
-    """
-    # get image as numpy array
-    im = np.array(image)
-    # get shape
-    w, h, d = im.shape
-    # change shape
-    im.shape = (w * h, d)
-    #mask = np.zeros(im.shape[0], dtype=bool)
-    #for i, item in enumerate(im):
-    #    mask[i] = (item.tolist() != [0,0,0,0])
-    #print(len(mask[mask==True]))
-    # get average
-    return tuple(np.average(im, axis=0))
+def get_average_rgbn(image):
+    image_array = np.array(image)
+    w, h, d = image_array.shape
+    image_array.shape = (w * h, d)
+    return tuple(np.average(image_array, axis=0))
 
 
 def distance(x, y):
     return np.linalg.norm(x - y)
 
 
-def img_to_hearts(img):
-    hsize = 10, 10
-
-    im = {}
+def img_to_hearts(image):
     im_np_arr = {}
 
-    im['black'] = Image.open('/home/alex/Desktop/hearts_vk/hearts/black4.png')
-    im['blue'] = Image.open('/home/alex/Desktop/hearts_vk/hearts/blue.png')
-    im['red'] = Image.open('/home/alex/Desktop/hearts_vk/hearts/red.png')
-    im['purple'] = Image.open('/home/alex/Desktop/hearts_vk/hearts/purple.png')
-    im['yellow'] = Image.open('/home/alex/Desktop/hearts_vk/hearts/yellow.png')
-    im['green'] = Image.open('/home/alex/Desktop/hearts_vk/hearts/green.png')
-
-
-    mean = {'black': np.array([ 29.05,  27.42,  36.87]),
-            'blue': np.array([  20.13,   99.44,  174.79]),
-            'red': np.array([ 162.48,   33.22,   32.08]),
-            'purple': np.array([ 104.5 ,   23.92, 131.09]),
-            'yellow': np.array([ 197.36,  159.94,   40.95]),
-            'green': np.array([  89.02,  134.15,   49.83])}
-
     for key, value in im.items():
-        im[key] = im[key].resize((hsize[0], hsize[1]), Image.ANTIALIAS)
+        im[key] = im[key].resize((heart_size[0], heart_size[1]), Image.ANTIALIAS)
         im_np_arr[key] = np.array(im[key])
-    imgChange = img
-    size = list(imgChange.size)
-    imgChange = imgChange.resize((size[0] - size[0] % hsize[0],
-                                  size[1] - size[1] % hsize[1])) #resize image
-    size = list(imgChange.size) #new size of image
+    img_change = image
+    size = list(img_change.size)
+    img_change = img_change.resize((size[0] - size[0] % heart_size[0],
+                                    size[1] - size[1] % heart_size[1]))  # resize image
+    size = list(img_change.size)  # new size of image
     print(size)
-    count_sq = (size[0] // hsize[0], size[1] // hsize[0])
+    # count_sq = (size[0] // hsize[0], size[1] // hsize[0])
 
-    img_array = np.array(imgChange)
+    img_array = np.array(img_change)
     size = img_array.shape[0:2]
-    count_sq = (size[0] // hsize[0], size[1] // hsize[0]) #count segments
+    count_sq = (size[0] // heart_size[0], size[1] // heart_size[0])  # count segments
 
-    for i in range(count_sq[0]*count_sq[1]): #loop for all squeres
+    for i in range(count_sq[0] * count_sq[1]):  # loop for all squeres
         s_w, s_h = i % count_sq[0], i // count_sq[0]
-        imslice = img_array[s_w*hsize[0]:s_w*hsize[0]+hsize[0], \
-                            s_h*hsize[1]:s_h*hsize[1]+hsize[1]] #copy slice of squre
+        imslice = img_array[s_w * heart_size[0]:s_w * heart_size[0] + heart_size[0], \
+                  s_h * heart_size[1]:s_h * heart_size[1] + heart_size[1]]  # copy slice of squre
         imslice = imslice.copy()
         w, h, d = imslice.shape
-        imslice.shape = (w * h, d) #reshape slice for np.average
+        imslice.shape = (w * h, d)  # reshape slice for np.average
 
         slicemean = np.array(tuple(np.average(imslice, axis=0)))
 
         mindist = float("inf")
         keymin = ""
-        for key, value in im.items(): #search what heart is the closest
+        for key, value in im.items():  # search what heart is the closest
             dist = distance(slicemean, mean[key])
-            if (mindist > dist):
+            if mindist > dist:
                 mindist = dist
                 keymin = key
-        for i in range(hsize[0]): #copy heart to image
-            for j in range(hsize[1]):
+        for i in range(heart_size[0]):  # copy heart to image
+            for j in range(heart_size[1]):
                 if im_np_arr[keymin][i][j][3] > 15:
-                    img_array[s_w*hsize[1]+i][s_h*hsize[1]+j] = im_np_arr[keymin][i][j][0:3]
+                    img_array[s_w * heart_size[1] + i][s_h * heart_size[1] + j] = im_np_arr[keymin][i][j][0:3]
     return Image.fromarray(img_array)
 
 
